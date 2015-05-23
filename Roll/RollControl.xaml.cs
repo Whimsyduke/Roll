4da2dbc6284eeae6a4314df0ff033f88d0c4dd5e
whimsyduke <whimsyduke@gmail.com>
2015-05-23 20:22:20 +0800

    0.0.1

---
+++ Roll/RollControl.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Roll
+{
+    /// <summary>
+    /// RollControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class RollControl : UserControl
+    {
+        ListView RollList;
+        public RollControl()
+        {
+            InitializeComponent();
+        }
+
+        public RollControl(ListView listView, int index)
+        {
+            InitializeComponent();
+            this.Index.Content = index;
+            RollList = listView;
+        }
+
+        public RollControl(ListView listView, XElement xml)
+        {
+            InitializeComponent();
+            this.Index.Content = xml.Attribute("Index").Value;
+            this.MinValue.Text = xml.Attribute("MinValue").Value;
+            this.MaxValue.Text = xml.Attribute("MaxValue").Value;
+            RollList = listView;
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsNumberic())
+                this.CountValue.Text = (System.Int32.Parse(this.CountValue.Text) - 1).ToString();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsNumberic())
+                this.CountValue.Text = (System.Int32.Parse(this.CountValue.Text) + 1).ToString();
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            this.CountValue.Text = "0";
+        }
+        
+        private bool IsNumberic()
+        {
+            if (string.IsNullOrEmpty(this.CountValue.Text))
+            {
+                MessageBox.Show("第" + this.Index.Content + "行计数数值为空！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            try
+            {
+                System.Int32.Parse(this.CountValue.Text);
+            }
+            catch
+            {
+                MessageBox.Show("第" + this.Index.Content + "行计数数值非整数数字！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        public XElement ToXML()
+        {
+            try
+            {
+                System.Int32.Parse(this.MinValue.Text);
+                System.Int32.Parse(this.MaxValue.Text);
+            }
+            catch
+            {
+                MessageBox.Show("第" + this.Index.Content + "行配置无效！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+            XElement root = new XElement("Index_" + Index.Content.ToString(),
+                new XAttribute("Index", this.Index.Content),
+                new XAttribute ("MinValue", this.MinValue.Text),
+                new XAttribute ("MaxValue", this.MaxValue.Text)
+                );
+            return root;
+        }
+
+        public void Generation(int seed)
+        {
+            int min, max;
+            try
+            {
+                min = System.Int32.Parse(this.MinValue.Text) - 1;
+                max = System.Int32.Parse(this.MaxValue.Text) + 1;
+            }
+            catch
+            {
+                MessageBox.Show("第" + this.Index.Content + "行最小值最大值无效！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (min > max - 2)
+            {
+                MessageBox.Show("第" + this.Index.Content + "行设置的最小值大于最大值！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Random ran = new Random(seed);
+            int value = ran.Next(min, max);
+            while (value == max || value == min)
+            {
+                value = ran.Next(min, max);
+            }
+            RanValue.Content = value;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            RollList.Items.Remove(this);
+            int i = 1;
+            foreach (RollControl select in RollList.Items)
+            {
+                select.Index.Content = i;
+                i++;
+            }
+        }
+    }
+}
---
+++ Roll/RollMainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Roll
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XElement m_Xml;
+        private static Random Ran;
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (File.Exists("config.ini"))
+            {
+                m_Xml = XElement.Load("config.ini");
+                this.RollNumber.Text = m_Xml.Attribute("RollNumber").Value;
+                try
+                {
+                    int rollNum = System.Int32.Parse(this.RollNumber.Text);
+                    for (int i = 1; i <= rollNum; i++)
+                    {
+                        RollList.Items.Add(new RollControl(RollList, m_Xml.Element("Index_" + i.ToString())));
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("配置文件出错，恢复初始设置！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    RollList.Items.Clear();
+                    this.RollNumber.Text = "1";
+                    RollList.Items.Add(new RollControl(RollList, 1));
+                    ToXML();
+                }
+            }
+            else
+            {
+                this.RollNumber.Text = "1";
+                RollList.Items.Add(new RollControl(RollList, 1));
+                ToXML();
+            }
+            Ran = new Random();
+        }
+
+        private void ToXML()
+        {
+            m_Xml = new XElement("Config", 
+                new XAttribute("RollNumber", RollNumber.Text));
+            foreach (RollControl roll in RollList.Items)
+            {
+                m_Xml.Add(roll.ToXML());
+            }
+            m_Xml.Save("config.ini");
+        }
+        
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            int rollNumber;
+            try
+            {
+                rollNumber = System.Int32.Parse(this.RollNumber.Text);
+            }
+            catch
+            {
+                MessageBox.Show("生成数量的内容非整数数字！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (rollNumber < 0)
+            {
+                MessageBox.Show("生成数量不能为负！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (rollNumber > 50)
+            {
+                MessageBox.Show("生成数量不能大于50！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (RollList.Items.Count > rollNumber)
+            {
+                while (rollNumber != RollList.Items.Count)
+                {
+                    RollList.Items.RemoveAt(rollNumber);
+                }
+            }
+            else if (RollList.Items.Count < rollNumber)
+            {
+                for (int i = RollList.Items.Count + 1; i <= rollNumber; i++)
+                {
+                    RollList.Items.Add(new RollControl(RollList, i));
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            ToXML();
+        }
+
+        private void Generation_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (RollControl roll in RollList.Items)
+            {
+                roll.Generation(Ran.Next());
+            }
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (RollList.Items.Count > 10)
+            {
+                RollList.Height = 310;
+            }
+            else
+            {
+                RollList.Height = Double.NaN;
+            }
+        }
+        
+    }
+}
