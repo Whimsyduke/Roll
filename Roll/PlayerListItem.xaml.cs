90d0a0d8336ee2a4514434f1a413397d35005dae
whimsyduke <whimsyduke@gmail.com>
2015-05-24 21:41:00 +0800

    1.0.2 添加D&D的Roll功能

---
+++ Roll/PlayerListItem.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roll
+{
+    /// <summary>
+    /// PlayerListItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class PlayerListItem : ComboBoxItem
+    {
+        MainWindow Main;
+        ComboBox PlayerList;
+
+        public PlayerListItem()
+        {
+            InitializeComponent();
+        }
+        public PlayerListItem(MainWindow window, ComboBox playerList, string name)
+        {
+            InitializeComponent();
+            PlayerName.Content = name;
+            Main = window;
+            PlayerList = playerList;
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            Main.DnDToXML(false, this.PlayerName.Content.ToString());
+            PlayerList.Items.Remove(this);
+        }
+    }
+}
--- Roll/RollMainWindow.xaml.cs
+++ Roll/RollMainWindow.xaml.cs
@@ -23,21 +23,40 @@ namespace Roll
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string FILE_NAME_LOG = "DnDRoll.log";
+        const string FILE_NAME_CONFIG = "config.ini";
         XElement m_Xml;
         private static Random Ran;
+        StreamWriter sw;
+        
         public MainWindow()
         {
             InitializeComponent();
-            if (File.Exists("config.ini"))
+            if (File.Exists(FILE_NAME_CONFIG))
             {
-                m_Xml = XElement.Load("config.ini");
-                this.RollNumber.Text = m_Xml.Attribute("RollNumber").Value;
+                m_Xml = XElement.Load(FILE_NAME_CONFIG);
                 try
                 {
+                    int openPageIndex = System.Int32.Parse(m_Xml.Attribute("OpenPage").Value);
+                    if (openPageIndex != 0 && openPageIndex != 1) throw new Exception();
+                    SetOpenPageIndex(openPageIndex);
+
+                    XElement rollConfig = m_Xml.Element("RollPageConfig");
+                    this.RollNumber.Text = rollConfig.Attribute("RollNumber").Value;
                     int rollNum = System.Int32.Parse(this.RollNumber.Text);
-                    for (int i = 1; i <= rollNum; i++)
+                    for (int i = 1; i < rollNum; i++)
                     {
-                        RollList.Items.Add(new RollControl(RollList, m_Xml.Element("Index_" + i.ToString())));
+                        RollList.Items.Add(new RollControl(RollList, rollConfig.Element("Index_" + i.ToString())));
+                    }
+                    if (rollNum == 0)
+                    {
+                        RollList.Items.Add(new RollControl(RollList, 1));
+                        this.RollNumber.Text = "1";
+                    }
+                    XElement dnDConfig = m_Xml.Element("DnDPageConfig");
+                    foreach (XElement select in dnDConfig.Elements())
+                    {
+                        PlayerNameList.Items.Add(new PlayerListItem(this, PlayerNameList, select.Value));
                     }
                 }
                 catch
@@ -46,7 +65,26 @@ namespace Roll
                     RollList.Items.Clear();
                     this.RollNumber.Text = "1";
                     RollList.Items.Add(new RollControl(RollList, 1));
-                    ToXML();
+                }
+                try
+                {
+                    if (!File.Exists(FILE_NAME_LOG))
+                    {
+                        sw = File.CreateText(FILE_NAME_LOG);
+                    }
+                    else
+                    {
+                        StreamReader sr = new StreamReader(FILE_NAME_LOG);
+                        DndLogTextBox.Text = sr.ReadToEnd();
+                        sr.Close();
+                        sw = new StreamWriter(FILE_NAME_LOG, true);
+                    }
+                    sw.AutoFlush = true;
+                }
+                catch
+                {
+                    MessageBox.Show("日志文件" + FILE_NAME_LOG + "被占用！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
                 }
             }
             else
@@ -57,17 +95,111 @@ namespace Roll
             Ran = new Random();
         }
 
-        private void ToXML()
+        private void RollToXML()
         {
-            m_Xml = new XElement("Config", 
-                new XAttribute("RollNumber", RollNumber.Text));
-            foreach (RollControl roll in RollList.Items)
+            if (File.Exists(FILE_NAME_CONFIG))
             {
-                m_Xml.Add(roll.ToXML());
+                m_Xml = XElement.Load(FILE_NAME_CONFIG);
+                m_Xml.Element("RollPageConfig").Remove();
+                XElement rollConfig = new XElement("RollPageConfig",
+                    new XAttribute("RollNumber", RollNumber.Text));
+                m_Xml.Add(rollConfig);
+                foreach (RollControl roll in RollList.Items)
+                {
+                    rollConfig.Add(roll.ToXML());
+                }
+                m_Xml.Element("RollPageConfig").Remove();
+                m_Xml.Add(rollConfig);
             }
-            m_Xml.Save("config.ini");
+            else
+            {
+                m_Xml = new XElement("Config",
+                    new XAttribute("OpenPage", 0));
+                XElement rollConfig = new XElement("RollPageConfig",
+                    new XAttribute("RollNumber", RollNumber.Text));
+                m_Xml.Add(rollConfig);
+                foreach (RollControl roll in RollList.Items)
+                {
+                    rollConfig.Add(roll.ToXML());
+                }
+                XElement DndConfig = new XElement("DnDPageConfig");
+                m_Xml.Add(DndConfig);
+            }
+            m_Xml.Save(FILE_NAME_CONFIG);
         }
-        
+
+        private void ConfigToXML()
+        {
+            if (File.Exists(FILE_NAME_CONFIG))
+            {
+                m_Xml = XElement.Load(FILE_NAME_CONFIG);
+                m_Xml.Attribute("OpenPage").Value = GetOpenPageIndex().ToString();
+            }
+            else
+            {
+                m_Xml = new XElement("Config",
+                    new XAttribute("OpenPage", GetOpenPageIndex().ToString()));
+                XElement rollConfig = new XElement("RollPageConfig",
+                    new XAttribute("RollNumber", 0));
+                m_Xml.Add(rollConfig);
+                XElement dnDConfig = new XElement("DnDPageConfig");
+                m_Xml.Add(dnDConfig);
+            }
+            m_Xml.Save(FILE_NAME_CONFIG);
+        }
+
+        public void DnDToXML(bool add, string name)
+        {
+
+            if (File.Exists(FILE_NAME_CONFIG))
+            {
+                m_Xml = XElement.Load(FILE_NAME_CONFIG);
+                XElement dnDConfig = m_Xml.Element("DnDPageConfig");
+                if (add)
+                {
+                    dnDConfig.Add(new XElement("_" + name.GetHashCode().ToString(), name));
+                }
+                else
+                {
+                    dnDConfig.Element("_" + name.GetHashCode().ToString()).Remove();
+                }
+            }
+            else
+            {
+                m_Xml = new XElement("Config",
+                    new XAttribute("OpenPage", 0));
+                XElement rollConfig = new XElement("RollPageConfig",
+                    new XAttribute("RollNumber", 0));
+                m_Xml.Add(rollConfig);
+                XElement dnDConfig = new XElement("DnDPageConfig");
+                if (add) dnDConfig.Add(new XElement("_" + name.GetHashCode().ToString(), name));
+                m_Xml.Add(dnDConfig);
+            }
+            m_Xml.Save(FILE_NAME_CONFIG);
+        }
+
+        public void DnDCleanToXML()
+        {
+
+            if (File.Exists(FILE_NAME_CONFIG))
+            {
+                m_Xml = XElement.Load(FILE_NAME_CONFIG);
+                XElement dnDConfig = m_Xml.Element("DnDPageConfig");
+                dnDConfig.RemoveAll();
+            }
+            else
+            {
+                m_Xml = new XElement("Config",
+                    new XAttribute("OpenPage", 0));
+                XElement rollConfig = new XElement("RollPageConfig",
+                    new XAttribute("RollNumber", 0));
+                m_Xml.Add(rollConfig);
+                XElement dnDConfig = new XElement("DnDPageConfig");
+                m_Xml.Add(dnDConfig);
+            }
+            m_Xml.Save(FILE_NAME_CONFIG);
+        }
+
         private void Confirm_Click(object sender, RoutedEventArgs e)
         {
             int rollNumber;
@@ -108,7 +240,7 @@ namespace Roll
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            ToXML();
+            RollToXML();
         }
 
         private void Generation_Click(object sender, RoutedEventArgs e)
@@ -131,5 +263,199 @@ namespace Roll
             }
         }
         
+        private int GetOpenPageIndex()
+        {
+            if (RollRadio.IsChecked == true) return 0;
+            if (DnDRadio.IsChecked == true) return 1;
+            return 0;
+        }
+
+        private void SetOpenPageIndex(int index)
+        {
+            MainTabControl.SelectedIndex = index;
+            switch (index)
+            {
+                case 0:
+                    RollRadio.IsChecked = true;
+                    break;
+                case 1:
+                    DnDRadio.IsChecked = true;
+                    break;
+            }
+        }
+
+        private void AboutSave_Click(object sender, RoutedEventArgs e)
+        {
+            ConfigToXML();
+        }
+
+        private void NewPlayerNameTextBox_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (NewPlayerNameTextBox.Text == "Add New Player ")
+            {
+                NewPlayerNameTextBox.Text = "";
+                NewPlayerNameTextBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void NewPlayerNameTextBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (NewPlayerNameTextBox.Text == "")
+            {
+                NewPlayerNameTextBox.Text = "Add New Player ";
+                NewPlayerNameTextBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void ConfirmPlayerName_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i < PlayerNameList.Items.Count; i++)
+            {
+                if ((PlayerNameList.Items[i] as PlayerListItem).PlayerName.Content.ToString() == NewPlayerNameTextBox.Text)
+                {
+                    MessageBox.Show("已经存在的人物名称！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            PlayerNameList.Items.Add(new PlayerListItem(this, PlayerNameList, NewPlayerNameTextBox.Text));
+            DnDToXML(true, NewPlayerNameTextBox.Text);
+            NewPlayerNameTextBox.Text = "Add New Player ";
+        }
+
+        private void PlayerNameList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NewPlayerPanel == null) return;
+            if (PlayerNameList.SelectedIndex == 0)
+            {
+                NewPlayerPanel.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                NewPlayerPanel.Visibility = System.Windows.Visibility.Collapsed;
+            }
+        }
+
+        private void DnDRoll_Click(object sender, RoutedEventArgs e)
+        {
+            int rollTime, rollNum, additionalValue;
+            try
+            {
+                rollTime = System.Int32.Parse(this.DndRollFirstBox.Text);
+                rollNum = System.Int32.Parse(this.DndRollSecondBox.Text);
+                additionalValue = System.Int32.Parse(this.AdditionalValue.Text);
+                if (rollTime < 0 || rollNum < 0) throw new Exception();
+            }
+            catch
+            {
+                MessageBox.Show("骰子设置错误，请检查后重试！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string playerName;
+            if (PlayerNameList.SelectedIndex == 0)
+            {
+                playerName = "";
+            }
+            else
+            {
+                playerName = (PlayerNameList.SelectedItem as PlayerListItem).PlayerName.Content.ToString();
+            }
+            string rollValue = "[";
+            int value = 0;
+            Random ran = new Random();
+            for (int i = rollTime; i > 0; i--)
+            {
+                int temp = ran.Next(0, rollNum + 1);
+                while (temp == -1 || temp == rollNum + 1)
+                {
+                    temp = ran.Next(0, rollNum + 1);
+                }
+                value += temp;
+                rollValue += temp + ",";
+            }
+            if (rollTime != 0)
+            {
+                rollValue = rollValue.Substring(0, rollValue.Count() - 1) + "]";
+            }
+            else
+            {
+                rollValue = "[]";
+            }
+            string additionalString = "";
+            if (additionalValue < 0)
+            {
+                additionalString = "-" + System.Math.Abs(additionalValue);
+            }
+            else
+            {
+                additionalString = "+" + additionalValue;
+            }
+            string fullLog = "[" + System.DateTime.Now.ToString() + "][" + playerName + "][" + ForTextBox.Text + "][" + rollTime + "d" + rollNum + additionalString + "] > " + value + rollValue;
+            string shortLog = "[" + System.DateTime.Now.ToLongTimeString() + "][" + playerName + "][" + ForTextBox.Text + "][" + rollTime + "d" + rollNum + "] > " + value + "\n";
+            if (FullRadio.IsChecked == true) DndLogTextBox.Text += fullLog + "\n";
+            if (ShortRadio.IsChecked == true) DndLogTextBox.Text += shortLog;
+
+            sw.WriteLine(fullLog); 
+        }
+
+        private bool IsNumberic()
+        {
+            if (string.IsNullOrEmpty(this.AdditionalValue.Text))
+            {
+                MessageBox.Show("附加计数数值为空！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            try
+            {
+                System.Int32.Parse(this.AdditionalValue.Text);
+            }
+            catch
+            {
+                MessageBox.Show("附加计数数值非整数数字！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsNumberic())
+                this.AdditionalValue.Text = (System.Int32.Parse(this.AdditionalValue.Text) - 1).ToString();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsNumberic())
+                this.AdditionalValue.Text = (System.Int32.Parse(this.AdditionalValue.Text) + 1).ToString();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            sw.Close();
+        }
+
+        private void ClearLog_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("确认是否清屏？\n清屏之前的内容依旧保存在log.txt中。", "提示", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                sw.WriteLine("----------------------------------------------");
+                DndLogTextBox.Text = "";
+            }
+        }
+
+        private void DnDReset_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("确认是否重置？重置会清除所有记录以及玩家名。\n此过程不可挽回。", "提示", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+            {
+                sw.Close();
+                DndLogTextBox.Text = "";
+                File.Delete(FILE_NAME_LOG);
+                sw = File.CreateText(FILE_NAME_LOG);                
+                while (PlayerNameList.Items.Count > 1)
+                {
+                    PlayerNameList.Items.RemoveAt(1);
+                }
+                DnDCleanToXML();
+            }
+        }
     }
 }
